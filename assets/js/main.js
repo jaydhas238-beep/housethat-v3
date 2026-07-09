6dcafdfe602b6c6a91459af35334826b7225d124
jaydhas238-beep <jaydhas238@gmail.com>
2026-07-09 16:08:33 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
+window.addEventListener("scroll", () => {
 
+    const header = document.querySelector(".header");
+
+    if(window.scrollY > 60){
+        header.classList.add("scrolled");
+    }else{
+        header.classList.remove("scrolled");
+    }
+
+});
